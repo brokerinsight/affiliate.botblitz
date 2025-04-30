fefcdbc5d7ff59c39b21e27406c7484d978167af
brokerinsight <martinngothokeli@gmail.com>
2025-04-30 22:29:11 +0000

    Configured Tailwind with input.css and config updates

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],  // make sure it scans both HTML and JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
