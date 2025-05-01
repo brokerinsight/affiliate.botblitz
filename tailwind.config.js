c72976a3d7f5736220ef6f4183cce8e8c66b4832
brokerinsight <martinngothokeli@gmail.com>
2025-05-02 02:30:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],  // make sure it scans both HTML and JS files
+  content: ['./public/**/*.{html,js}', './src/**/*.{jsx,js}'],  // make sure it scans both HTML and JS files
   theme: {
     extend: {},
   },
