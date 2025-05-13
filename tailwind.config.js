828baeebdd92a728105568c8765282a75bcc260a
brokerinsight <martinngothokeli@gmail.com>
2025-05-13 00:36:01 +0000

    Reset project files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./public/**/*.{html,js}', './src/**/*.{jsx,js}'],  // make sure it scans both HTML and JS files
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
